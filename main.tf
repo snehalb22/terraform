03f07a66a14d6a6c68c8b34eaef4b64209f0b495
unknown <snehalbaviskar2@gmail.com>
2022-11-27 02:03:12 +0000

    change in tf

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-west-2"
+  region = "eu-west-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-09d3b3274b6c5d4aa"
+resource "aws_instance" "AWS-instance" {
+  count = 1
+  ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "Node1"
   }
+
 }
