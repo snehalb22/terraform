ea41558a34ab1628a452884f311b008b6cf511bc
unknown <snehalbaviskar2@gmail.com>
2022-11-27 01:58:06 +0000

    change in tf file

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
 
   tags = {
