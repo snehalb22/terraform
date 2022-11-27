140eb650e7d11df97b194723ad33c9cc5201bd5d
unknown <snehalbaviskar2@gmail.com>
2022-11-27 02:09:51 +0000

    count added in terraformfile

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
-provider "aws" {
-  region = "eu-west-1"
-}
-
-resource "aws_instance" "AWS-instance" {
-  count = 1
-  ami           = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Node1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
 
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  count = 5
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
